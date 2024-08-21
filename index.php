bf5d9ee93806cdde642709d0aa9b828772c7ce12
bcelikbas10 <bcelikbas8@gmail.com>
2024-08-21 10:33:57 +0300

    Staj PHP Variables Alanı tamamlandı.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    echo"i like pizza <br>";
+    echo"it's really good";
+    // br bir alt satıra geçirmek için kullanılır.
+
+?>
+</head>
+</body>
+    <button>order pizza</button>
+
+</body>
+<?
+    $name = "elma armut";
+    $food = "pizza";
+    $email = "sahte123@gmail.com";
+
+    $age = 21;
+    $users = 2;
+
+    echo"hello {$name}<br>";
+    echo"you like {$food}<br>";
+    echo"your email is {$email}<br>";
+    
+    echo"you are {$age} years old<br>";
+    echo"there are {$users} users online<br>";
+?>
