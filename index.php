8d1992cbc2d1ca36b40d7365eb9dc45f1270b17d
bcelikbas10 <bcelikbas8@gmail.com>
2024-08-22 09:40:11 +0300

    get ve post arasındaki farklar

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
     <button>order pizza</button>
 
 </body>
-<?
+<?php
     $name = "elma armut";
     $food = "pizza";
     $email = "sahte123@gmail.com";
-    $pizza = "sucuklu";
+    $pizza = "kaşarlı";
 
     $age = 21;
     $users = 2;
@@ -26,4 +26,49 @@
     echo"there are {$users} users online<br>";
 
     echo $$food;
+?>
+<?php
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    //$z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    //$z = $x ½ $y;
+    echo $z;
+?>
+<?php
+    $counter = 0;
+
+    $counter++;
+    echo $counter;
+?>
+<?php
+    //işlem önceliği
+    // ()
+    // **
+    // * / ½
+    // + -
+
+    $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    echo $total;
+?>
+</head>
+<body>
+    <form action="index.php">
+    <label>username:</label><br>
+    <input type="text" name="username"><br>
+    <label>password:</label><br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="log in">
+</form>
+</body>
+</html>
+
+<?php
+    echo "{$_POST["username"]} <br>";
+    echo "{$_POST["password"]} <br>";
 ?>
